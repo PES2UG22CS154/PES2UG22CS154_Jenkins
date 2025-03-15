eee6f4e28ed4906fccc2a6b29611cb640b623098
PES2UG22CS154 <pes2ug22cs154@pesu.pes.edu>
2025-03-16 00:08:55 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, PES2UG22CS154\n";
+  cout << "Hello, C\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
